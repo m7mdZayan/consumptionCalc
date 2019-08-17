4c166e4d1599531f0e2813558369bf55b18687b5
Mohamed Zayan <m7mdzayan@gmail.com>
2019-08-17 16:09:45 +0200

    program has been finished

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int X;
+    double Y,C;
+    cin >> X >> Y;
+    C = X / Y;
+    cout << fixed << setprecision(3) << C << " km/l" << endl;
+    return 0;
+}
